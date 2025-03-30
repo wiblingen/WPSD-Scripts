f5d7a5c226d57e950c995d671b56ae56be2a1ce6
Chipster <go@w.ay>
2025-03-30 01:19:58 -0500

    another nx text refactor

--- .wpsd-nextion-text.py
+++ .wpsd-nextion-text.py
@@ -4,12 +4,15 @@
 # (C) 2025, by Lucas Burns, AE0LI; Chip Cuccio, W0CHP
 #
 
+
 import serial
 import sys
 import os
 
 MODEM_BAUDRATE = 115200
 MMDVM_SERIAL = 0x80
+
+NEXTION_FIELDS = ["t0", "t1", "t2", "t5", "t20", "t30", "t31", "t32" ] # <https://repo.w0chp.net/WPSD-Dev/WPSD_Nextion/src/branch/main/Nextion_Field_Use.md#nextion-display-fields>
 
 def MakeNextionCommand(commandString: str):
     result = bytearray()
@@ -18,18 +21,11 @@ def MakeNextionCommand(commandString: str):
     return result
 
 def MakeSetTextCommandString(field, value):
-    commandString = field
-    commandString += ".txt=\""
-    commandString += value
-    commandString += "\""
-    return commandString
+    return f"{field}.txt=\"{value}\""
 
 def MakeModemCommand(nextionCommand: bytearray):
     frameLength = len(nextionCommand) + 3
-    result = bytearray()
-    result.append(0xe0)
-    result.append(frameLength)
-    result.append(MMDVM_SERIAL)
+    result = bytearray([0xe0, frameLength, MMDVM_SERIAL])
     result.extend(nextionCommand)
     return result
 
@@ -40,24 +36,37 @@ def SetTextValue(field, value, serialInterface: serial.Serial):
     command = MakeModemCommand(MakeNextionCommand(MakeSetTextCommandString(field, value)))
     SendModemCommand(command, serialInterface)
 
-if __name__ == "__main__":
+def ClearAllFields(serialInterface: serial.Serial):
+    for field in NEXTION_FIELDS:
+        SetTextValue(field, "", serialInterface)
+    SendModemCommand(MakeModemCommand(MakeNextionCommand("ref 0")), serialInterface)  # Refresh display
 
+if __name__ == "__main__":
     programPath = sys.argv[0]
     programName = os.path.basename(programPath)
 
-    if (len(sys.argv) < 4):
-        print(f"Usage: {programName} <port> <field> <text value>")
+    if len(sys.argv) < 3:
+        print(f"Usage: {programName} <port> [-c | <field> <text value>]")
         sys.exit()
 
     port = sys.argv[1]
-    field = sys.argv[2]
-    textValue = sys.argv[3]
 
     try:
-        serialInterface = serial.Serial(port = port, baudrate=MODEM_BAUDRATE)
-        SetTextValue(field, textValue, serialInterface=serialInterface)
+        serialInterface = serial.Serial(port=port, baudrate=MODEM_BAUDRATE)
+        
+        if sys.argv[2] == "-c":
+            ClearAllFields(serialInterface)
+        elif len(sys.argv) == 4:
+            field = sys.argv[2]
+            textValue = sys.argv[3]
+            SetTextValue(field, textValue, serialInterface)
+        else:
+            print(f"Invalid arguments. Usage: {programName} <port> [-c | <field> <text value>]")
+            sys.exit()
+        
         serialInterface.close()
-
+    
     except serial.SerialException as e:
         print(f"Serial port exception: {e}")
         sys.exit()
+
